ece722099987f5b0d89c87651d710e6b3183469c
Anushree Bagchi <anushreebagchi4791@gmail.com>
2018-08-24 02:02:00 +0530

    Styles added.

--- js/script.js
+++ js/script.js
@@ -8,26 +8,61 @@ var ViewModel = function (){
     
     var self=this;
     this.markers=[];
-    // Creating markers 
-        for  (var i=0;i<model.locations.length;i++)
-        {
-            
-            this.markerTitle=model.locations[i].title;
-            this.markerLocation=model.locations[i].location;
-            this.marker= new google.maps.Marker({
-                                map: map,
-                                title: this.markerTitle,
-                                position:this.markerLocation,
-                                animation: google.maps.Animation.DROP
-            });
-            this.markers.push(this.marker);              
-        };
-
+    var defaulticon = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
+    var hovericon ='http://maps.google.com/mapfiles/ms/icons/green-dot.png';
     
+    //Creating InfoWindow
 
+    var infowindow=new google.maps.InfoWindow();
+
+    // Creating markers 
+    for  (let i=0;i<model.locations.length;i++)
+    {
+        
+        this.markerTitle=model.locations[i].title;
+        this.markerLocation=model.locations[i].location;
+        this.marker= new google.maps.Marker({
+                            map: map,
+                            title: this.markerTitle,
+                            position:this.markerLocation,
+                            animation: google.maps.Animation.DROP,
+                            icon: defaulticon
+        });
+        this.marker.addListener('mouseover',function (){
+            this.setIcon(hovericon);
+            this.setAnimation(google.maps.Animation.BOUNCE);
+        });
+        this.marker.addListener('mouseout',function (){
+            this.setIcon(defaulticon);
+            this.setAnimation();
+        });
+        this.markers.push(this.marker);                                   
+    };
+    //creating listener so that when  a marker is clicked the info window is displayed.
+    for (let i=0;i<self.markers.length;i++)
+    {
+        this.markers[i].addListener('click',function (){
+            populateInfoWindow(self.markers[i],infowindow);
+        });
+    };
+    
+    function populateInfoWindow (marker, infowindow){
+        //make sure info window is not already  opened in this marker.
+        if (infowindow.marker!=marker)
+        {
+            infowindow.marker=marker;
+            infowindow.setContent(marker.title);
+            infowindow.open(map,marker);
+            // clear marker property when info window is closed.
+            infowindow.addListener('closeclick', function (){
+                infowindow.setMarker(null);
+            });
+        }
+        
+    }
     this.searchItem = ko.observable('');
     this.mapList = ko.observableArray([]);
-    for (var i=0;i<model.locations.length;i++){
+    for (let i=0;i<model.locations.length;i++){
         var item=model.locations[i];
         self.mapList.push(item);
     };  
@@ -36,9 +71,9 @@ var ViewModel = function (){
         var searchFilter = self.searchItem().toLowerCase();
         if (searchFilter) {            
                 var result=[];
-                for (var i =0;i<self.mapList().length;i++)
+                for (let i =0;i<self.mapList().length;i++)
                 {
-                    var str = self.mapList()[i].title.toLowerCase();
+                    let str = self.mapList()[i].title.toLowerCase();
                     if (str.includes(searchFilter)){
                         result.push(self.mapList()[i]);
                     }
@@ -49,7 +84,7 @@ var ViewModel = function (){
                 return self.mapList();
             };
     });
-    console.log(this.markers);
+    
     
     //Displaying markers on the location list
     //1.create computed array on observable array
@@ -57,18 +92,19 @@ var ViewModel = function (){
         var list= self.locationList();
         var markerArray=self.markers;
         //clear map with all the markers
-        for (var i=0;i<markerArray.length;i++){
+        for (let i=0;i<markerArray.length;i++){
             markerArray[i].setMap(null);            
         };
         //for loop through list and show markers for the list
-        for (var j=0;j<markerArray.length;j++){
-            for (var k=0;k<list.length;k++){
+        for (let j=0;j<markerArray.length;j++){
+            for (let k=0;k<list.length;k++){
                 if (list[k].title===markerArray[j].title){
                     markerArray[j].setMap(map);
                 }
             }           
         };
     }, self);
+ 
     
     
 };
